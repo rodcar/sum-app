376a598c8d3809b9020ffea89d8509c31dd16c7d
rodcar <ivanrod12@hotmail.com>
2023-02-01 15:50:04 -0500

    commit inicial

---
+++ src/app/sum-form/sum-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sum-form',
+  templateUrl: './sum-form.component.html',
+  styleUrls: ['./sum-form.component.css']
+})
+export class SumFormComponent {
+
+}
